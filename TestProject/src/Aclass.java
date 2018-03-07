bf7ee8d5da8c142f9b28860aae585c7d4011cc94
Leegunmin <dudrjsxo3@naver.com>
2018-03-07 11:35:46 +0900

    A클래스 수정

--- TestProject/src/Aclass.java
+++ TestProject/src/Aclass.java
@@ -1,4 +1,24 @@
 
 public class Aclass {
+	public static void main(String[] args){
+		Car mycar;
+		mycar = new Car();
+		mycar.setCar("hello", 5, 4);
+		mycar.print();
+	}
+}
 
+class Car{
+	String car;
+	int speed;
+	int gear;
+	
+	void print(){
+		System.out.println(car+"//"+speed+"//"+gear);
+	}
+	void setCar(String c,int a,int b){
+		car = c;
+		speed = a;
+		gear = b;
+	}
 }
